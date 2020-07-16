c9b32f141e00eb48ef7119011916abe09cda4a06
unknown <kanakulyajameson@gmail.com>
2020-07-16 12:13:46 +0400

    Updates

---
+++ precache-manifest.c65b73c4cb2dc310cf271c2767857c08.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ea4df33d2bf02fee113f1a62f1c3f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c45155fc532a26ed4fc3",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5fa705fc479ce0fec4a2",
+    "url": "/static/css/main.6c708ca0.chunk.css"
+  },
+  {
+    "revision": "c45155fc532a26ed4fc3",
+    "url": "/static/js/2.9a4b65b7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9a4b65b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa705fc479ce0fec4a2",
+    "url": "/static/js/main.fa3cf935.chunk.js"
+  },
+  {
+    "revision": "45e4e59b82965c50ce7f",
+    "url": "/static/js/runtime-main.3e637d78.js"
+  },
+  {
+    "revision": "1b87eb3aa96e4b56462f7bf0139f27f7",
+    "url": "/static/media/Geometric.1b87eb3a.jpg"
+  },
+  {
+    "revision": "259e21e2edd030b19a2182c289b21f7d",
+    "url": "/static/media/assorted.259e21e2.jpg"
+  },
+  {
+    "revision": "e914cb4f57d8c9ea4c4e5ebea8d94834",
+    "url": "/static/media/author.e914cb4f.jpeg"
+  },
+  {
+    "revision": "a637f072262357759eedbe4bf08ccb6c",
+    "url": "/static/media/desktop.a637f072.jpg"
+  },
+  {
+    "revision": "30890571fd20cb0d0f338f3ddd7db2c6",
+    "url": "/static/media/logo.30890571.png"
+  }
+]);
